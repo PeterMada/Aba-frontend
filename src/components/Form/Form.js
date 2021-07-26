da24078c4f756c356015d639452504637cb82e72
Peter <petermada@gmail.com>
2021-07-26 23:04:03 +0200

    Netify test

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+//import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 import cS from './Form.module.scss';
@@ -31,117 +31,37 @@ export default ({ personId }) => {
         </GoogleReCaptchaProvider >
     */
 
+    return (
+        <form name={`contact-form-${personId}`} method="post" data-netlify="true">
+            <div class="field">
+                <label>Nom *</label>
+                <div class="control">
+                    <input type="text" class="input is-medium" placeholder="Nom" name="firstname" required />
+                </div>
+            </div>
+            <div class="field">
+                <label>Prénom *</label>
+                <div class="control">
+                    <input type="text" class="input is-medium" placeholder="Prénom" name="lastname" required />
+                </div>
+            </div>
+            <div class="field">
+                <label>Adresse email *</label>
+                <div class="control">
+                    <input type="mail" class="input is-medium" placeholder="Adresse email" name="email" required />
+                </div>
+            </div>
+            <div class="field">
+                <label>Téléphone</label>
+                <div class="control">
+                    <input type="tel" class="input is-medium" placeholder="Téléphone" name="phone" minlength="10" />
+                </div>
+            </div>
+            <input type="submit" class="button is-fullwidth secondary-btn is-rounded" value="Commencer" />
+        </form>
+    );
+
 
     // @TODO #5 @PeterMada
-    return (
 
-        <>
-
-            <div>
-                <div className={cS.message}>{message}</div>
-                <Formik
-                    initialValues={{ Jmeno: '', Text: '', Email: '', Interest: 'Terapie_Supervize', therapist: personClearId }}
-                    validate={values => {
-                        const errors = {};
-                        if (!values.Jmeno) {
-                            errors.Jmeno = 'Vyplňte prosím vaše jméno a příjmení.';
-                        }
-
-                        if (!values.Text) {
-                            errors.Text = 'Toto pole nemůže být prázdné.';
-                        }
-
-                        if (!values.Email) {
-                            errors.Email = 'Toto pole nemůže být prázdné.';
-                        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.Email)) {
-                            errors.Email = 'Nesprávný formát emailu.';
-                        }
-
-                        return errors;
-                    }}
-
-                    onSubmit={(values, actions) => {
-                        const Url = window.location.pathname;
-                        const updateValues = { ...values, Url };
-                        const subitJson = JSON.stringify(updateValues, null, 2);
-                        console.log(updateValues);
-
-                        const apiUrlFirstPart = process.env.GATSBY_API_URL;
-                        console.log(encode({ "form-name": `contact-form-${personId}`, ...values }));
-
-                        fetch('/', {
-                            method: "POST",
-                            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                            body: encode({ "form-name": `contact-form-${personId}`, ...values })
-                        })
-                            .then(() => {
-                                alert('Success');
-                                actions.resetForm()
-                            })
-                            .catch(() => {
-                                alert('Error');
-                            })
-                            .finally(() => actions.setSubmitting(false))
-
-                        /*
-                    fetch(`${apiUrlFirstPart}/emails`, {
-                        method: 'POST',
-                        body: subitJson,
-                        headers: {
-                            'Content-type': 'application/json; charset=UTF-8'
-                        }
-                    }).then(function (response) {
-                        if (response.ok) {
-                            setMessage(succesfulMessage);
-                        }
-                    }).catch(function (error) {
-                        setMessage(errorMessage);
-                    });
-                    */
-
-
-
-                        actions.setSubmitting(false);
-                        actions.resetForm({ Jmeno: '', Text: '', Email: '', Interest: 'Terapie_Supervize', therapist: personClearId });
-                    }}
-                >
-                    {({ isSubmitting }) => (
-                        <Form netlify-honeypot="bot-field" className={cS.wrap} method="post" data-netlify="true" name={`contact-form-${personId}`}>
-                            <Field type="hidden" name="form-name" value={`contact-form-${personId}`} />
-                            <Field type="hidden" name="bot-field" />
-                            <input type="hidden" name="cnjp" />
-                            <div className={`${cS.row} ${cS.rowHalf}`}>
-                                <label htmlFor="Jmeno" className={cS.label}>Jméno</label>
-                                <Field type='text' name='Jmeno' className={cS.input} />
-                                <ErrorMessage name='Jmeno' component="span" className={cS.error} />
-                            </div>
-                            <div className={`${cS.row} ${cS.rowHalf}`}>
-                                <label htmlFor="Email" className={cS.label}>Email</label>
-                                <Field type='email' name='Email' className={cS.input} />
-                                <ErrorMessage name='Email' component="span" className={cS.error} />
-                            </div>
-                            <div className={`${cS.row} ${cS.rowHalf}`}>
-                                <label htmlFor="Interest" className={cS.label}>Mám zájem o</label>
-                                <Field name="Interest" as="select" className={`${cS.input} ${cS.select}`}>
-                                    <option value="Terapie_Supervize">Terapie/Supervize</option>
-                                    <option value="Jine">Jiné</option>
-                                </Field>
-                            </div>
-                            <div className={cS.row}>
-                                <label htmlFor="Text" className={cS.label}>Zpráva</label>
-                                <Field type='text' name='Text' as='textarea' className={`${cS.input} ${cS.textArea}`} />
-                                <ErrorMessage name='Text' component='span' className={cS.error} />
-                            </div>
-                            <Field type='hidden' name='therapist' value={personClearId} />
-                            <div className={cS.buttonWrap}>
-                                <button type='submit' disabled={isSubmitting} className={cS.button}>
-                                    Odeslat
-                                </button>
-                            </div>
-                        </Form>
-                    )}
-                </Formik>
-            </div>
-        </>
-    );
 }
